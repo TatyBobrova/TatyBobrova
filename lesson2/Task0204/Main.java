1e6e7c6fbe3bd01a582d3a67fbf16401a2d25a68
Taty.Bobrova <taty.bobrova@x5.ru>
2020-09-21 18:42:03 +0300

    урок 2 задание 4

---
+++ lesson2/Task0204/Main.java
@@ -0,0 +1,20 @@
+package Task0204;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int d;
+        d = sc.nextInt();
+        if
+        (d > 0)
+            System.out.println(d + 1);
+        else if
+        (d<0)
+            System.out.println(d-2);
+        else
+            System.out.println(10);
+
+    }
+}
