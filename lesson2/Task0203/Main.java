dd7e4cf7d4e792331cc4be43fad4cc773b87f92c
Taty.Bobrova <taty.bobrova@x5.ru>
2020-09-21 18:30:16 +0300

    урок 2 задание 3

---
+++ lesson2/Task0203/Main.java
@@ -0,0 +1,17 @@
+package Task0203;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int d;
+        d = sc.nextInt();
+        if
+        (d>=0)
+            d=d+1;
+        else
+            d=d;
+        System.out.println(d);
+    }
+}
