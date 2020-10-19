a587a69e02b53bdd4f0d139921d546f4991ca8ca
Taty.Bobrova <taty.bobrova@x5.ru>
2020-10-19 11:25:52 +0300

    урок 4 задание square

--- lesson4/Interfeys/Interfeys.java
+++ lesson4/Interfeys/Interfeys.java
@@ -4,9 +4,9 @@ public class Interfeys {
 
     public static void main(String[] args) {
         Shape[] shape = new Shape[3];
-        shape[0] = new Triangle();
-        shape[1] = new Circle();
-        shape[2] = new Rectangle();
+        shape[0] = new Triangle(5,4);
+        shape[1] = new Circle(6);
+        shape[2] = new Rectangle(7,5);
         for (Shape sh: shape){
             System.out.println("Площадь фигуры: "+sh.square());
         }
